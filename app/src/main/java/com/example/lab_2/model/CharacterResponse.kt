2db23b6f3b133ee46b68f4dc84672596dab0a18a
OldM1lk <MrlaimWin@gmail.com>
2025-03-03 20:47:06 +0700

    Minor changes

--- app/src/main/java/com/example/lab_2/model/CharacterResponse.kt
+++ app/src/main/java/com/example/lab_2/model/CharacterResponse.kt
@@ -1,7 +1,5 @@
 package com.example.lab_2.model
 
-import com.example.lab_2.model.Character
-
 data class CharacterResponse(
     val results: List<Character>
 )
--- app/src/main/java/com/example/lab_2/view/CharacterView.kt
+++ app/src/main/java/com/example/lab_2/view/CharacterView.kt
@@ -1,29 +1,41 @@
 package com.example.lab_2.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import com.example.lab_2.viewModel.CharacterViewModel
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import com.example.lab_2.viewModel.CharacterViewModel
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
@@ -35,7 +47,13 @@ fun CharacterScreen(viewModel: CharacterViewModel = viewModel()) {
             TopAppBar(
                 title = { Text(text = "Rick and Morty") },
                 actions={
-                    IconButton(onClick = {viewModel.fetchCharacters()}) { Icon(Icons.Default.Refresh, contentDescription = "О приложении")}
+                    IconButton(
+                        onClick = {
+                            viewModel.fetchCharacters()
+                        }
+                    ) {
+                        Icon(Icons.Default.Refresh, contentDescription = "Обновить")
+                    }
                 }
             )
         }
@@ -55,8 +73,8 @@ fun CharacterScreen(viewModel: CharacterViewModel = viewModel()) {
 @Composable
 fun CharacterList(characters: List<com.example.lab_2.model.Character>, modifier: Modifier = Modifier) {
     LazyColumn(modifier = modifier) {
-        itemsIndexed(characters) { index, character ->
-            CharacterItem(character)
+        items(characters) {
+            CharacterItem(it)
         }
     }
 }
