9436365f01d0d097fdac330767ca6e7a59ecf777
OldM1lk <MrlaimWin@gmail.com>
2025-03-15 18:15:44 +0700

    Hotfix#1

---
+++ app/src/main/java/com/example/lab_2/ui/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.lab_2.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lab_2.ui.theme.Lab_2Theme
+import com.example.lab_2.viewModel.CharacterViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab_2Theme(darkTheme = true) {
+                CharacterScreen()
+            }
+        }
+    }
+}
+
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun AppTopBar(viewModel: CharacterViewModel) {
+    TopAppBar(
+        title = { Text(text = "Rick and Morty") },
+        actions = {
+            IconButton(
+                onClick = {
+                    viewModel.fetchCharacters()
+                }
+            ) {
+                Icon(Icons.Default.Refresh, contentDescription = "Обновить")
+            }
+        }
+    )
+}
+
+@Composable
+@Preview(showBackground = true)
+fun CharacterViewPreview() {
+    Lab_2Theme(darkTheme = true) {
+        CharacterScreen()
+    }
+}
